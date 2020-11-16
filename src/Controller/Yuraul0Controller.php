fffe0f733ba5658408104dbf5e74f7ba6c6ebef4
Yuraul <yuraulio@gmail.com>
2020-11-16 11:38:24 +0200

    Implement form behavior.

--- src/Controller/Yuraul0Controller.php
+++ src/Controller/Yuraul0Controller.php
@@ -1,30 +1,41 @@
 <?php
 
-/**
- * @file
- * Contains \Drupal\yuraul0\Controller\Yuraul0Controller.
- *
- *  Constructs a guestbook page
- */
-
 namespace Drupal\yuraul0\Controller;
 
+/**
+ * Constructs a guestbook page.
+ */
 class Yuraul0Controller {
 
   /**
    * {@inheritdoc}
    */
   public function feedback() {
-
-//    return [
-//      '#markup' => 'Done',
-//    ];
-
-    return [
+    $query = \Drupal::database()->select('guestbook');
+    $query->fields('guestbook', ['username', 'message']);
+//    $query->addExpression('COUNT(*)');
+//    $count = $query->execute()->fetchField();
+    $result = $query->execute()->fetchAll();
+    foreach ($result as $user) {
+      $users[] = [
+        '#type' => 'markup',
+        '#markup' => "
+          <ul style=\"color: deepskyblue; \">
+            <li>$user->username</li>
+            <li>$user->message</li>
+          </ul>",
+      ];
+    }
+    $users[] = [
       '#type' => 'markup',
       '#markup' => t('<div style="color: deepskyblue;">This is my module!</div>'),
       'form' =>  \Drupal::formBuilder()->getForm('Drupal\yuraul0\Form\AddFeedback'),
     ];
+    //Deleting records if it's too much
+    if (count($result) > 3) {
+      \Drupal::database()->delete('guestbook')->execute();
+    }
+    return $users;
   }
 
   public function admin() {
--- src/Form/AddFeedback.php
+++ src/Form/AddFeedback.php
@@ -1,16 +1,6 @@
 <?php
 
-/**
- * @file
- * Contains Drupal\yuraul0\Form\AddFeedback.
- *
- * Implements a form with AJAX validation for adding feedback
- */
-
 namespace Drupal\yuraul0\Form;
-
-use Drupal\Core\File\FileSystemInterface;
-use Drupal\Core\File\File;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -18,6 +8,9 @@ use Drupal\Core\Ajax\AjaxResponse;
 use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Ajax\RedirectCommand;
 
+/**
+ * Implements a form with AJAX validation for adding feedback.
+ */
 class AddFeedback extends FormBase {
 
   /**
@@ -39,34 +32,39 @@ class AddFeedback extends FormBase {
     $form['username'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Your name'),
+      '#description' => $this->t('Only letters, numbers and underscore, please. Up to 100 symbols'),
       '#required' => TRUE,
     ];
 
     $form['email'] = [
       '#type' => 'email',
       '#title' => $this->t('E-mail'),
+      '#description' => $this->t('Only letters, numbers and underscore in account name. Only letters and "." (dot) in domain.'),
 //      '#required' => TRUE,
     ];
 
     $form['phone'] = [
       '#type' => 'tel',
       '#title' => $this->t('Phone number'),
+      '#description' => $this->t('International format (+XXXXYYYYYYYYYYYY, X = 1-4 digits)'),
 //      '#required' => TRUE,
     ];
 
     $form['message'] = [
       '#type' => 'textarea',
       '#title' => $this->t('Your feedback message'),
+      '#description' => $this->t('Up to 500 symbols.'),
 //      '#required' => TRUE,
     ];
 
     $form['avatar'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Add your profile picture'),
+      '#description' => $this->t('Only *.jpeg, *.jpg, *.png, up to 2MB.'),
       '#upload_location' => 'public://yuraul0/user',
       '#multiple' => FALSE,
       '#upload_validators' => [
-        'file_validate_is_image' => array(),
+        'file_validate_is_image' => [],
         'file_validate_extensions' => ['png jpg jpeg'],
         'file_validate_size' => [2097152],
       ],
@@ -75,6 +73,7 @@ class AddFeedback extends FormBase {
     $form['picture'] = [
       '#type' => 'managed_file',
       '#title' => $this->t('Add picture to your feedback'),
+      '#description' => $this->t('Only *.jpeg, *.jpg, *.png, up to 5MB.'),
       '#upload_location' => 'public://yuraul0/post',
       '#multiple' => FALSE,
       '#upload_validators' => [
@@ -119,9 +118,8 @@ class AddFeedback extends FormBase {
       'message' => $form_state->getValue('message'),
       'timestamp' => time(),
     ])->execute();
-    \Drupal::messenger()->addMessage($this->t('Thank you @name, your phone number is @number', [
+    \Drupal::messenger()->addMessage($this->t('Thank you @name, for your feedback!', [
       '@name' => $form_state->getValue('username'),
-      '@number' => $form_state->getValue('phone'),
     ]));
 
   }
@@ -131,22 +129,24 @@ class AddFeedback extends FormBase {
    */
   public function ajaxSubmitCallback(array &$form, FormStateInterface $form_state) {
     $ajax_response = new AjaxResponse();
-    $message = [
-      '#theme' => 'status_messages',
-      '#message_list' => \Drupal::messenger()->all(),
-      '#status_headings' => [
-        'status' => t('Status message'),
-        'error' => t('Error message'),
-        'warning' => t('Warning message'),
-      ],
-    ];
-    $messages = \Drupal::service('renderer')->render($message);
-//    \Drupal::messenger()->deleteAll();
-    $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
-//    if (count($form_state->getErrors() < 1)) {
-//      $ajax_response->addCommand(new RedirectCommand('https://yuraul/'));
-//    }
-//    \Drupal::messenger()->deleteAll();
+    if (count($form_state->getErrors()) === 0) {
+      $current_path = \Drupal::service('path.current')->getPath();
+      $ajax_response->addCommand(new RedirectCommand($current_path));
+      \Drupal::messenger()->deleteByType('error');
+    }
+    else {
+      $message = [
+        '#theme' => 'status_messages',
+        '#message_list' => \Drupal::messenger()->all(),
+        '#status_headings' => [
+          'status' => t('Status message'),
+          'error' => t('Error message'),
+          'warning' => t('Warning message'),
+        ],
+      ];
+      $messages = \Drupal::service('renderer')->render($message);
+      $ajax_response->addCommand(new HtmlCommand('#form-system-messages', $messages));
+    }
     return $ajax_response;
   }
 
